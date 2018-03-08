c040510cd986f28b2cf367680ad9da9cf35e7943
manisaisrinivask <manisaisrinivas1@gmail.com>
2018-03-07 23:47:48 -0600

    lab

--- lab-3/Code/NLP/nlp_bigrams.py
+++ lab-3/Code/NLP/nlp_bigrams.py
@@ -1,62 +1,36 @@
-        # importing libraries
-        import re, collections
-        from nltk.tokenize import word_tokenize, wordpunct_tokenize, sent_tokenize
-        from nltk.stem import SnowballStemmer, WordNetLemmatizer
-        from nltk.tag import pos_tag
-        import sys
-        import nltk
+        # LDA
 
-        # Reading the Text file
-        with open('sample_file.txt', 'r') as f:
-            File_read = f.read()
+        # Importing the libraries
+        import numpy as np
+        import matplotlib.pyplot as plt
+        import pandas as pd
 
+        # Importing the dataset
+        dataset = pd.read_csv('Wine.csv')
+        X = dataset.iloc[:, :3].values
+        y = dataset.iloc[:, 3].values
 
-        def tokens(text):
-            # Get all words from the corpus
-            return re.findall('[a-z]+', text.lower())
+        # Splitting the dataset into the Training set and Test set
+        from sklearn.model_selection import train_test_split
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
+        # Feature Scaling
+        from sklearn.preprocessing import StandardScaler
+        sc = StandardScaler()
+        X_train = sc.fit_transform(X_train)
+        X_test = sc.transform(X_test)
 
-        Words = tokens(File_read)
+        # Applying LDA
+        from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+        lda = LDA(n_components = 2)
+        X_train = lda.fit_transform(X_train, y_train)
+        X_test = lda.transform(X_test)
 
-        # Implementation of the Lemmatization
-        lemmatizer = WordNetLemmatizer()
-        LemmatizedWords = [lemmatizer.lemmatize(word) for word in Words]
-        print (LemmatizedWords)
+        # Fitting Logistic Regression to the Training set
+        from sklearn.linear_model import LogisticRegression
+        classifier = LogisticRegression(random_state = 0)
+        classifier.fit(X_train, y_train)
 
-        print ("\n------------------------------------------------")
+        # Predicting the Test set results
+        y_pred = classifier.predict(X_test)
 
-        # printing bi-grams
-        bigrm = list(nltk.bigrams(Words))
-        print("the bigrams are: ")
-        print (bigrm)
-
-        # Calculating the word frequency
-        WordCount = collections.Counter(bigrm)
-
-        Top5Words = WordCount.most_common(5)
-
-        TopWords = [word for (word, freq) in Top5Words]
-
-        print ('\n Top 5 bigrams with high frequency of occurrence: \n', str(TopWords))
-        # Implementing the word and sentence tokenization to extract the words in a sentence
-
-        Sentence = sent_tokenize(File_read)
-
-        print ('\nThe Sentences in the File are: \n', Sentence)
-
-        # Sentences with the Top5Words
-        lst = []
-        for s in Sentence:
-            Words = tokens(s)
-            bigrm = list(nltk.bigrams(Words))
-            for b in bigrm:
-                if str(b) in str(TopWords):
-                    lst.append(s)
-                    break
-        print ("===============================================")
-        print ("These are the sentences with top bigrams: ")
-        print (lst)
-        print("=====================")
-        # Summary of the text
-        print ("The summary is:")
-        print(" ".join(lst))
