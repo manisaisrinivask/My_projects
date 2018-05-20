d8818832a6b0417592c335eb5882a7db6b5f4c93
manisaisrinivask <manisaisrinivas1@gmail.com>
2018-05-20 11:19:41 -0500

    2

--- icp1/source/pr1.py
+++
@@ -1,3 +0,0 @@
-'''Write a Python program to get the Python version you are using'''
-
-print "python 3"
--- icp2/code/pr_3.py
+++
@@ -1,6 +0,0 @@
-filename="input_file"
-with open(filename,"r") as f:
-    for line in f.readlines():
-        words = len(line.split())
-        line=line.strip()
-        print(line,words)
