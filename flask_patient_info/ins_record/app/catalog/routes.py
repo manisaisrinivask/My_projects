a27a32fd81c01d3795c32c4d2193e98d51c5573c
manisaisrinivask <manisaisrinivas1@gmail.com>
2018-09-11 16:10:31 -0400

    small modification

--- flask_patient_info/ins_record/app/catalog/routes.py
+++ flask_patient_info/ins_record/app/catalog/routes.py
@@ -10,10 +10,16 @@ def index():
     return render_template('home1.html')
 
 
-
 @main.route("/forward/", methods=['POST'])
 def move_forward():
     #Moving forward code
     patients = Patient.query.all()
     forward_message = "Moving Forward..."
     return render_template('home.html', patients=patients)
+
+
+@main.route("/forwardinsurance/", methods=['POST'])
+def insurance_forward():
+    insurances = Insurance.query.all()
+    forward_message = "Going on....."
+    return render_template('homeinsurance.html', insurances=insurances)
